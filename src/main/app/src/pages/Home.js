f771eb88c5fdb193236903b1cce95e0669f33fc6
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-01-23 12:28:35 -0500

    Deleted unused imports

--- src/main/app/src/pages/Home.js
+++ src/main/app/src/pages/Home.js
@@ -1,9 +1,3 @@
-import { AppBar, Button, IconButton, Toolbar } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import NavBar from '../components/NavBar'
 
 function Home() {
