fae4385ddf83ff987d013c279c57b758f4805921
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-01-23 12:24:30 -0500

    Updated missing import statement from index.js

--- src/main/app/src/index.js
+++ src/main/app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
