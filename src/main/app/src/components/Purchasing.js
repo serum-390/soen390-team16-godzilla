c7455a9019148d09c23dfa9f7d5208baeb7a1a0b
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-02-09 04:07:06 -0500

    Added a dialog box for adding vendors in purchasing page

--- src/main/app/src/components/Purchasing.js
+++ src/main/app/src/components/Purchasing.js
@@ -1,7 +1,20 @@
-import { Button, makeStyles } from '@material-ui/core';
+import { Button, Dialog, makeStyles } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import React from 'react';
 import { SpinBeforeLoading } from './inventory/Inventory';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import MaskedInput from 'react-text-mask';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import PropTypes from 'prop-types';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -41,25 +54,166 @@ const orderRows = [
   { id: 4, vendorName: 'Vendor1', items: '????', timestamp: "01/31/2021", cost: "$100" }
 ];
 
-const LoadedView = () => {
+function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+      placeholderChar={'\u2000'}
+      showMask
+    />
+  );
+}
+
+TextMaskCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired,
+};
+
+
+const PhoneNumberField = () => {
+  const [values, setValues] = React.useState({
+    textmask: '(  )    -    ',
+    numberformat: '1320',
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <FormControl>
+        <InputLabel>Phone Number</InputLabel>
+        <Input
+          value={values.textmask}
+          onChange={handleChange}
+          name="textmask"
+          inputComponent={TextMaskCustom}
+        />
+      </FormControl>
+  );
+};
+
+
+const ProvinceDrawer = () =>{
+  const [province, setProvince] = React.useState('');
+
+  const handleChange = (event) => {
+    setProvince(event.target.value);
+  };
+  return(
+    <div  style={{ width: "250px" }}>
+      <FormControl>
+        <Select
+          value={province}
+          onChange={handleChange}
+        >
+          <MenuItem value={"QC"}>QC</MenuItem>
+          <MenuItem value={"ON"}>ON</MenuItem>SS
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+function LoadedView() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const AddCustomerDialogBox = () => {
+    return (
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth='sm'>
+        <DialogTitle>Add New Vendor</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please Do Not Leave Any Fields Empty!
+          </DialogContentText>
+          <InputLabel>Vendor Name</InputLabel>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            type="text"
+            />
+          <InputLabel>Address</InputLabel>
+          <TextField
+            autoFocus
+            margin="dense"
+            type="text"
+            multiline
+            rows={2}
+            fullWidth/>
+
+            <Grid container spacing={2}>
+            <Grid item md={3}>
+            <InputLabel>City</InputLabel>
+            <TextField
+            autoFocus
+            margin="dense"
+            type="text"/>
+            </Grid>
+            <Grid item md={3}>
+            <InputLabel>Postal Code</InputLabel>
+            <TextField
+            autoFocus
+            margin="dense"
+            type="text"/>
+            </Grid>
+            <Grid item md={3}>
+            <InputLabel>Province</InputLabel>
+            <ProvinceDrawer/>
+            </Grid>
+          </Grid>
+          <PhoneNumberField/>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => {
+            handleClose() ;
+            AddNewVendor();
+        }} color="primary">
+            Add Vendor
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+
   return (
     <div style={{ height: 600, width: '100%' }}>
       <h1 style={{ textAlign: "center" }}>Purchase Department</h1>
       <div style={{ height: 600, width: '45%', float: 'left' }}>
         <h2 style={{ float: 'left' }}>Vendors</h2>
-        <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={() => { AddNewVendor(); }}>
+        <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={handleClickOpen}>
           Add New Vendor
-                    </Button>
-        <DataGrid rows={vendorRows} columns={vendorColumns} pageSize={4} onRowClick={(newSelection) => { ShowVendorDetail(newSelection); }} />
+        </Button>
+        <AddCustomerDialogBox/>
+        <DataGrid rows={vendorRows} columns={vendorColumns} pageSize={4} onRowClick={(newSelection) => { ShowVendorDetail(newSelection); } } />
       </div>
       <div style={{ height: 600, width: '45%', float: 'right' }}>
         <div>
           <h2 style={{ float: 'left' }}>Purchase Orders</h2>
-          <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={() => { AddNewPurchaseOrder(); }}>
+          <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={() => { AddNewPurchaseOrder(); } }>
             Add New Purchase Order
-                    </Button>
+          </Button>
         </div>
-        <DataGrid rows={orderRows} columns={orderColumns} pageSize={4} onRowClick={(newSelection) => { ShowPurchaseOrderDetail(newSelection); }} />
+        <DataGrid rows={orderRows} columns={orderColumns} pageSize={4} onRowClick={(newSelection) => { ShowPurchaseOrderDetail(newSelection); } } />
       </div>
     </div>
   );
