2b1c2345213331f6fa0d426843135384a5446788
Sean Mckenzie <60203505+mckenzietsean@users.noreply.github.com>
2021-02-12 20:07:36 -0500

    Updated Calendar
    - Calendar is more polished and now knows if a cell has been clicked

--- src/main/app/src/components/Planning.js
+++ src/main/app/src/components/Planning.js
@@ -9,8 +9,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Calendar from './planning/Calendar';
-
-
+import { useState } from "react";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,129 +25,63 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function GenerateCalendarRows(){
-  var rows = [];
-  var cols = [];
-  for (var i = 1; i < 32; i++) {
-      // FOR TESTING
-      if(i === 6 || i === 21){
-        cols.push(<TableCell style={{backgroundColor:'#ba543f', height:'120px'}}>{i}</TableCell>);
-      }
-      else if(i === 16){
-        cols.push(<TableCell style={{backgroundColor:'#66cc79', height:'120px'}}>{i}</TableCell>);
-      }
-      else{
-        cols.push(<TableCell style={{height:'120px'}}>{i}</TableCell>);
-      }
-
-      // Row is filled
-      if(cols.length === 7){
-        rows.push(cols);
-        cols = [];
-      }
-  }
-
-  // Not empty
-  if(cols.length !== 0){
-    rows.push(cols);
-  }
-
-  return rows;
-}
-
-function ScheduleTable() {
-  const classes = useStyles();
-
-  var scheduleRows = GenerateCalendarRows();
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell colSpan='7'>
-              <h2 style={{textAlign:'center'}}>JANUARY 2020</h2>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Su</TableCell>
-            <TableCell>Mo</TableCell>
-            <TableCell>Tu</TableCell>
-            <TableCell>We</TableCell>
-            <TableCell>Th</TableCell>
-            <TableCell>Fr</TableCell>
-            <TableCell>Sa</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody >
-          {scheduleRows.map((row) => (
-            <TableRow key={row.name}>
-              {row[0]}
-              {row[1]}
-              {row[2]}
-              {row[3]}
-              {row[4]}
-              {row[5]}
-              {row[6]}
-            </TableRow>
-          ))}
-
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
-
 function createActivityData(title, content) {
   return { title, content };
 }
 
-const activityRows = [
-  createActivityData('Date', '20/01/2020'),
-  createActivityData('Activity Name', 'Placeholder Day'),
-  createActivityData('Activity Description', 'A day for temporary placeholders'),
-  createActivityData('Frequency', 'Monthly'),
-  createActivityData('Organiser Name', 'Jane Doe')
-];
-
-function ActivityTable() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableCell colSpan='2'>
-            <h2 style={{textAlign:'center'}}>Add to Schedule</h2>
-          </TableCell>
-        </TableHead>
-        <TableBody>
-          {activityRows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell>{row.title}</TableCell>
-              <TableCell>{row.content}</TableCell>
-            </TableRow>
-          ))}
-          <TableRow>
-            <TableCell colSpan='2'>
-              <TextField id="filled-basic" label="Notes" multiline rows={6} style={{ width: '100%'}}/>
-              <Button variant='contained' color='primary' style={{float: 'right', marginTop:'20px'}} onClick={() => { SavePlanning();}}>Save</Button>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
-
-
-
 const LoadedView = () => {
+  const [date, setDate] = useState();
+
+  const changeDay = (d, m, y) => {
+    m = (Number.parseInt(m, 10) +1);
+    //let day = new Date(m + "/" + d + "/" + y);
+    setDate(m + "/" + d + "/" + y); 
+    //alert(day);
+  }
+
+  const activityRows = [
+    createActivityData('Date', [date]),
+    createActivityData('Activity Name', 'Placeholder Day'),
+    createActivityData('Activity Description', 'A day for temporary placeholders'),
+    createActivityData('Frequency', 'Monthly'),
+    createActivityData('Organiser Name', 'Jane Doe')
+  ];
+
+  function ActivityTable() {
+    const classes = useStyles();
+  
+    return (
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableCell colSpan='2'>
+              <h2 style={{textAlign:'center'}}>Add to Schedule</h2>
+            </TableCell>
+          </TableHead>
+          <TableBody>
+            {activityRows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell>{row.title}</TableCell>
+                <TableCell>{row.content}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell colSpan='2'>
+                <TextField id="filled-basic" label="Notes" multiline rows={6} style={{ width: '100%'}}/>
+                <Button variant='contained' color='primary' style={{float: 'right', marginTop:'20px'}} onClick={() => { SavePlanning();}}>Save</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+
   return (
     <div style={{ height: 600, width: '100%' }}>
       <h1 style={{ textAlign: "center" }}>Planning Department</h1>
       <div style={{ height: 600, width: '55%', float: 'left' }}>
-        {/*<ScheduleTable></ScheduleTable>*/}
-        {<Calendar></Calendar>}
+        {<Calendar changeDay={changeDay}></Calendar>}
       </div>
       <div style={{ height: 600, width: '40%', float: 'right' }}>
 
--- src/main/app/src/components/planning/Calendar.js
+++ src/main/app/src/components/planning/Calendar.js
@@ -1,149 +1,8 @@
-/*import { Button, makeStyles, TextField } from '@material-ui/core';
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(2),
-  },
-  sort: {
-    margin: theme.spacing(1),
-    textTransform: 'none',
-  },
-  table: {
-    minWidth: 650,
-  }
-}));
-
-window.today = new Date();
-window.currentMonth = window.today.getMonth();
-window.currentYear = window.today.getFullYear();
-//window.selectYear = document.getElementById("year");
-//window.selectMonth = document.getElementById("month");
-window.selectYear = '2021';
-window.selectMonth = 'Feb';
-
-window.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-//window.monthAndYear = document.getElementById("monthAndYear");
-window.monthAndYear = "Feb 2021";
-showCalendar(window.currentMonth, window.currentYear);
-window.currentYear = '';
-
-function Next() {
-  window.currentYear = (window.currentMonth === 11) ? window.currentYear + 1 : window.currentYear;
-  window.currentMonth = (window.currentMonth + 1) % 12;
-  showCalendar(window.currentMonth, window.currentYear);
-}
-
-function Previous() {
-  window.currentYear = (window.currentMonth === 0) ? window.currentYear - 1 : window.currentYear;
-  window.currentMonth = (window.currentMonth === 0) ? 11 : window.currentMonth - 1;
-  showCalendar(window.currentMonth, window.currentYear);
-}
-
-function Jump() {
-  window.currentYear = parseInt(window.selectYear.value);
-  window.currentMonth = parseInt(window.selectMonth.value);
-  showCalendar(window.currentMonth, window.currentYear);
-}
-
-function ShowCalendar(month, year){
-    var firstDay = (new Date(year, month)).getDay();
-
-    // CLEAR ALL PREVIOUS TABLE ROWS
-
-    // FILL OUT DATA
-
-    var date = 1;
-    var table = [];
-    var row = [];
-    for (var i = 0; i < 6; i++) {
-
-      // CREATE TABLE ROW
-      row = [];
-
-      // CREATE INDIVIDUAL CELLS
-      for (var j = 0; j < 7; j++) {
-        if (i === 0 && j < firstDay) {
-          //
-          row.push(<TableCell style={{height:'120px'}}></TableCell>);
-        }
-        else if (date > daysInMonth(month, year)) {
-          break;
-        }
-        else{
-          row.push(<TableCell style={{height:'120px'}}>{date}</TableCell>);
-          date++;
-        }
-      }
-
-      // APPEND ROW TO CALENDAR BODY
-        table.push(row);
-    }
-}
-
-function daysInMonth(iMonth, iYear) 
-{ 
-    return 32 - new Date(iYear, iMonth, 32).getDate();
-}
-
-function ScheduleTable() {
-  const classes = useStyles();
-
-  //var scheduleRows = GenerateCalendarRows();
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell colSpan='7'>
-              <h2 style={{textAlign:'center'}}>JANUARY 2020</h2>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Su</TableCell>
-            <TableCell>Mo</TableCell>
-            <TableCell>Tu</TableCell>
-            <TableCell>We</TableCell>
-            <TableCell>Th</TableCell>
-            <TableCell>Fr</TableCell>
-            <TableCell>Sa</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody id={'calendar-body'}>
-          {scheduleRows.map((row) => (
-            <TableRow key={row.name}>
-              {row[0]}
-              {row[1]}
-              {row[2]}
-              {row[3]}
-              {row[4]}
-              {row[5]}
-              {row[6]}
-            </TableRow>
-          ))}
-
-        </TableBody>
-      </Table>
-      
-    </TableContainer>
-    
-  );
-}*/
-
 import { Button, makeStyles} from '@material-ui/core';
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
-import { useState, setState } from "react";
+import { useState} from "react";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -156,42 +15,11 @@ const useStyles = makeStyles(theme => ({
   },
   table: {
     minWidth: 650,
+  },
+  selectedCell: {
+    backgroundColor: '#ba543f'
   }
 }));
-
-function GenerateCalendarRows(){
-  var rows = [];
-  var cols = [];
-  for (var i = 1; i < 32; i++) {
-      // FOR TESTING
-      if(i === 6 || i === 21){
-        cols.push(<TableCell style={{backgroundColor:'#ba543f', height:'120px'}}>{i}</TableCell>);
-      }
-      else if(i === 16){
-        cols.push(<TableCell style={{backgroundColor:'#66cc79', height:'120px'}}>{i}</TableCell>);
-      }
-      else{
-        cols.push(<TableCell style={{height:'120px'}}>{i}</TableCell>);
-      }
-
-      // Row is filled
-      if(cols.length === 7){
-        rows.push(cols);
-        cols = [];
-      }
-  }
-
-  // Not empty
-  if(cols.length !== 0){
-    rows.push(cols);
-  }
-
-  return rows;
-}
-
-
-//////////////////////
-
 
 const row = (x,i) =>
   <TableRow key={x.name}>
@@ -204,15 +32,17 @@ const row = (x,i) =>
     {x[6]}
   </TableRow>;
 
-const Calendar = props => {
+const Calendar = ({ changeDay }) => {
   const classes = useStyles();
   const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const [month, setMonth] = useState(new Date().getMonth());
   const [year, setYear] = useState(new Date().getFullYear());
   const [monthYear, setMonthYear] = useState(monthNames[[month]] + " " + [year]);
   const [data, setData] = useState(ChangeCalendar([year], [month]));
-  
-  
+  //const [selectedDay, setSelectedDay] = useState(0);
+
+  //const [currentDay, setCurrentDay] = useState(new Date().getDay());
+    
   function PrevMonth(){
     let m = Number.parseInt([month], 10);
     let y = Number.parseInt([year], 10);
@@ -228,7 +58,7 @@ const Calendar = props => {
     let m = Number.parseInt([month], 10);
     let y = Number.parseInt([year], 10);
     let newMonth = (m + 1) % 12;   // FOR SOME REASON, ADDS +1 TO MONTH (1 becomes 11)
-    let newYear = (m == 11) ? y + 1 : y;
+    let newYear = (m === 11) ? y + 1 : y;
     setMonth(newMonth);
     setYear(newYear);
     setData(ChangeCalendar(newYear, newMonth));
@@ -256,13 +86,14 @@ const Calendar = props => {
       for (var j = 0; j < 7; j++) {
         if (i === 0 && j < firstDay) {
           //
-          rows.push(<TableCell style={{height:'120px'}}></TableCell>);
+          rows.push(<TableCell style={{height:'120px'} } ></TableCell>);
         }
         else if (date > daysInMonth(y, m)) {
           break;
         }
         else{
-          rows.push(<TableCell style={{height:'120px'}}>{date}</TableCell>);
+          rows.push(<TableCell style={{height:'120px'}} id={date} onClick={(e)=>{SelectDay(e, m, y);}}>{date}</TableCell>);
+          //cols.push(<TableCell style={{backgroundColor:'#ba543f', height:'120px'}}>{i}</TableCell>);
           date++;
         }
       }
@@ -274,6 +105,25 @@ const Calendar = props => {
     return table;
   }
 
+  function SelectDay(e, m, y){
+    //alert(e.target.id + "/" + (m+1) + "/" + y);
+    changeDay(e.target.id, m, y);
+    /*alert([selectedDay][1]);
+
+    if(Number.parseInt([selectedDay], 10) !== 0){
+      let prevSelected = [selectedDay][0];   
+      // remove class of prev
+      prevSelected.target.classList.remove("selectedCell");
+      alert('b');
+    }
+   
+    // add class to new
+    e.target.classList.add("selectedCell");
+    var t = [e.target, e.target.id, m, y];
+    alert(t);
+    setSelectedDay(t);*/
+  }
+
   return (
     <div>
        <TableContainer component={Paper}>
@@ -281,7 +131,11 @@ const Calendar = props => {
             <TableHead>
               <TableRow>
                 <TableCell colSpan='7'>
-                  <h2 style={{textAlign:'center'}}>{[monthYear]}</h2>
+                  <Button variant='contained' color='primary' style={{float: 'left', marginTop:'20px'}} onClick={() => { PrevMonth();}}>Previous</Button>
+                  <Button variant='contained' color='primary' style={{float: 'right', marginTop:'20px'}} onClick={()=>{NextMonth();}}>Next</Button>
+                  <h2 style={{float: 'center', textAlign:'center'}}>{[monthYear]}</h2>
+
+                  
                 </TableCell>
               </TableRow>
               <TableRow>
@@ -307,9 +161,7 @@ const Calendar = props => {
             })}
             </TableBody>
           </Table>
-        </TableContainer>
-        <Button variant='contained' color='primary' style={{float: 'left', marginTop:'20px'}} onClick={() => { PrevMonth();}}>Previous</Button>
-        <Button variant='contained' color='primary' style={{float: 'right', marginTop:'20px'}} onClick={()=>{NextMonth();}}>Next</Button>
+        </TableContainer> 
     </div>
    
   );
