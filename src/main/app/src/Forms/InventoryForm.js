450390d1583f10f9126db1171818fa010c9f2e39
Yasaman <Yasamanarya80@yahoo.com>
2021-03-03 13:19:59 -0500

    fixed indentation

--- src/main/app/src/Forms/InventoryForm.js
+++ src/main/app/src/Forms/InventoryForm.js
@@ -10,137 +10,137 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default function InventoryForm(props) {
-    const item = props.onSubmit(null, false);
-    const [open, setOpen] = React.useState(false);
-    const [location, setLocation] = React.useState("");
-    const [itemName, setItemName] = React.useState("");
-    const [sellPrice, setSellPrice] = React.useState("");
-    const [buyPrice, setBuyPrice] = React.useState("");
-    const [quantity, setQuantity] = React.useState("");
-    const [goodType, setGoodType] = React.useState("");
-    const [billOfMaterial, setBillOfMaterial] = React.useState(item.billOfMaterial);
+  const item = props.onSubmit(null, false);
+  const [open, setOpen] = React.useState(false);
+  const [location, setLocation] = React.useState("");
+  const [itemName, setItemName] = React.useState("");
+  const [sellPrice, setSellPrice] = React.useState("");
+  const [buyPrice, setBuyPrice] = React.useState("");
+  const [quantity, setQuantity] = React.useState("");
+  const [goodType, setGoodType] = React.useState("");
+  const [billOfMaterial, setBillOfMaterial] = React.useState(item.billOfMaterial);
 
 
-    const handleClickOpen = () => {
-        setOpen(true);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    let data = {
+      id: "",
+      itemName: itemName,
+      goodType: goodType,
+      quantity: quantity,
+      sellPrice: sellPrice,
+      buyPrice: buyPrice,
+      location: location,
+      billOfMaterial: billOfMaterial
     };
+    props.onSubmit(data, true);
+    setOpen(false);
+    // want to update the table after clicking this
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  return (
+    <div>
+      <Button variant="contained" color="primary" style={{float: 'right'}} onClick={handleClickOpen}>
+        {props.initialButton}
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">{props.dialogTitle}</DialogTitle>
 
-    const handleSubmit = () => {
-        let data = {
-            id: "",
-            itemName: itemName,
-            goodType: goodType,
-            quantity: quantity,
-            sellPrice: sellPrice,
-            buyPrice: buyPrice,
-            location: location,
-            billOfMaterial: billOfMaterial
-        };
-        props.onSubmit(data, true);
-        setOpen(false);
-        // want to update the table after clicking this
-    };
-
-    return (
-        <div>
-            <Button variant="contained" color="primary" style={{float: 'right'}} onClick={handleClickOpen}>
-                {props.initialButton}
-            </Button>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">{props.dialogTitle}</DialogTitle>
-
-                <DialogContent>
-                    <DialogContentText>
-                        {props.dialogContentText}
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="itemName"
-                        label="Item Name"
-                        type="string"
-                        defaultValue={item.itemName}
-                        onChange={(event) => setItemName(event.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="location"
-                        label="Location"
-                        type="string"
-                        defaultValue={item.location}
-                        onChange={(event) => setLocation(event.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="quantity"
-                        label="Quantity"
-                        type="number"
-                        defaultValue={item.quantity}
-                        onChange={(event) => setQuantity(event.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="sellPrice"
-                        label="Sell Price"
-                        type="number"
-                        step={0.5}
-                        defaultValue={item.sellPrice}
-                        onChange={(event) => setSellPrice(event.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="buyPrice"
-                        label="Buy Price"
-                        type="number"
-                        step={0.5}
-                        defaultValue={item.buyPrice}
-                        onChange={(event) => setBuyPrice(event.target.value)}
-                        fullWidth
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="goodType"
-                        label="Good Type"
-                        onChange={(event) => setGoodType(event.target.value)}
-                        fullWidth
-                        type="number"
-                        defaultValue={item.goodType}
-                        InputProps={{inputProps: {min: 1, max: 6}}}
-                    />
-                    <FormControlLabel
-                        control={<Checkbox
-                            autoFocus
-                            margin="dense"
-                            id="billOfMaterial"
-                            checked={billOfMaterial}
-                            onChange={(event) => setBillOfMaterial(event.target.checked)}
-                            fullWidth
-                        />}
-                        label="Bill of Material"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleSubmit} color="primary">
-                        {props.submitButton}
-                    </Button>
-                    <Button onClick={handleClose} color="primary">
-                        Cancel
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+        <DialogContent>
+          <DialogContentText>
+            {props.dialogContentText}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="itemName"
+            label="Item Name"
+            type="string"
+            defaultValue={item.itemName}
+            onChange={(event) => setItemName(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="location"
+            label="Location"
+            type="string"
+            defaultValue={item.location}
+            onChange={(event) => setLocation(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="quantity"
+            label="Quantity"
+            type="number"
+            defaultValue={item.quantity}
+            onChange={(event) => setQuantity(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="sellPrice"
+            label="Sell Price"
+            type="number"
+            step={0.5}
+            defaultValue={item.sellPrice}
+            onChange={(event) => setSellPrice(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="buyPrice"
+            label="Buy Price"
+            type="number"
+            step={0.5}
+            defaultValue={item.buyPrice}
+            onChange={(event) => setBuyPrice(event.target.value)}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="goodType"
+            label="Good Type"
+            onChange={(event) => setGoodType(event.target.value)}
+            fullWidth
+            type="number"
+            defaultValue={item.goodType}
+            InputProps={{inputProps: {min: 1, max: 6}}}
+          />
+          <FormControlLabel
+            control={<Checkbox
+              autoFocus
+              margin="dense"
+              id="billOfMaterial"
+              checked={billOfMaterial}
+              onChange={(event) => setBillOfMaterial(event.target.checked)}
+              fullWidth
+            />}
+            label="Bill of Material"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSubmit} color="primary">
+            {props.submitButton}
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
--- src/main/app/src/components/inventory/Inventory.js
+++ src/main/app/src/components/inventory/Inventory.js
@@ -9,212 +9,212 @@ import axios from "axios";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        padding: theme.spacing(1),
-    },
-    sort: {
-        margin: theme.spacing(1),
-        textTransform: 'none',
-    },
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+  },
+  sort: {
+    margin: theme.spacing(1),
+    textTransform: 'none',
+  },
 }));
 
 const updateItem = async data => {
-    try {
-        const api = `/api/inventory/${data.id}`;
-        const updated = await axios.put(api, data);
-        console.log(`STATUS CODE: ${updated.status}`);
-        console.log(`DATA: ${updated.data || "Nothing"}`);
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const api = `/api/inventory/${data.id}`;
+    const updated = await axios.put(api, data);
+    console.log(`STATUS CODE: ${updated.status}`);
+    console.log(`DATA: ${updated.data || "Nothing"}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 const insertItem = async data => {
-    try {
-        const api = `/api/inventory/`;
-        const inserted = await axios.post(api, data);
-        console.log(`STATUS CODE: ${inserted.status}`);
-        console.log(`DATA: ${inserted.data || "Nothing"}`);
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const api = `/api/inventory/`;
+    const inserted = await axios.post(api, data);
+    console.log(`STATUS CODE: ${inserted.status}`);
+    console.log(`DATA: ${inserted.data || "Nothing"}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 
 const deleteItem = async id => {
-    try {
-        const api = `/api/inventory/${id}`;
-        const inserted = await axios.delete(api);
-        console.log(`STATUS CODE: ${inserted.status}`);
-        console.log(`DATA: ${inserted.data || "Nothing"}`);
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const api = `/api/inventory/${id}`;
+    const inserted = await axios.delete(api);
+    console.log(`STATUS CODE: ${inserted.status}`);
+    console.log(`DATA: ${inserted.data || "Nothing"}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 
 const inventoryCols = [
-    {field: 'id', headerName: 'Item', width: 100},
-    {field: 'ItemName', headerName: 'Item Name', width: 130},
-    {field: 'GoodType', headerName: 'Type', width: 100},
-    {field: 'Quantity', headerName: 'Quantity', width: 130},
-    {field: 'SellPrice', headerName: 'Sell Price', width: 130},
-    {field: 'BuyPrice', headerName: 'Buy Price', width: 130},
-    {field: 'Location', headerName: 'Location', width: 130},
-    {field: 'BillOfMaterial', headerName: 'BOM', width: 100},
-    {
-        field: 'modify',
-        headerName: 'Modify',
-        width: 100,
-        renderCell: params => (
-            <div style={{margin: 'auto'}}>
-                {
-                    <InventoryForm
-                        initialButton='Edit'
-                        dialogTitle='Inventory Information '
-                        dialogContentText='Please enter any information you would like to modify: '
-                        submitButton='Update'
-                        onSubmit={params.value}
-                    />
-                }
-            </div>
-        ),
-    },
-    {
-        field: 'delete',
-        headerName: 'Delete',
-        width: 130,
-        renderCell: params => (
-            <div style={{margin: 'auto'}}>
-                <Button variant="contained" onClick={params.value} color="primary" style={{float: 'right'}}>
-                    Delete
-                </Button>
-            </div>
-        ),
-    }
+  {field: 'id', headerName: 'Item', width: 100},
+  {field: 'ItemName', headerName: 'Item Name', width: 130},
+  {field: 'GoodType', headerName: 'Type', width: 100},
+  {field: 'Quantity', headerName: 'Quantity', width: 130},
+  {field: 'SellPrice', headerName: 'Sell Price', width: 130},
+  {field: 'BuyPrice', headerName: 'Buy Price', width: 130},
+  {field: 'Location', headerName: 'Location', width: 130},
+  {field: 'BillOfMaterial', headerName: 'BOM', width: 100},
+  {
+    field: 'modify',
+    headerName: 'Modify',
+    width: 100,
+    renderCell: params => (
+      <div style={{margin: 'auto'}}>
+        {
+          <InventoryForm
+            initialButton='Edit'
+            dialogTitle='Inventory Information '
+            dialogContentText='Please enter any information you would like to modify: '
+            submitButton='Update'
+            onSubmit={params.value}
+          />
+        }
+      </div>
+    ),
+  },
+  {
+    field: 'delete',
+    headerName: 'Delete',
+    width: 130,
+    renderCell: params => (
+      <div style={{margin: 'auto'}}>
+        <Button variant="contained" onClick={params.value} color="primary" style={{float: 'right'}}>
+          Delete
+        </Button>
+      </div>
+    ),
+  }
 
 ];
 
 
 const getInventory = async () => {
-    const api = '/api/inventory/';
-    const got = await fetch(api);
-    const json = await got.json();
-    return json || [];
+  const api = '/api/inventory/';
+  const got = await fetch(api);
+  const json = await got.json();
+  return json || [];
 };
 
 const FilledInventoryView = ({inventoryItems, classes}) => {
-    let items = [];
+  let items = [];
 
-    let updateRow = (item, updatedItem, toUpdate) => {
-        if (toUpdate) {
-            updateItem({
-                id: item.id,
-                itemName: updatedItem.itemName === "" ? item.itemName : updatedItem.itemName,
-                goodType: updatedItem.goodType === "" ? item.goodType : updatedItem.goodType,
-                quantity: updatedItem.quantity === "" ? item.quantity : updatedItem.quantity,
-                sellPrice: updatedItem.sellPrice === "" ? item.sellPrice : updatedItem.sellPrice,
-                buyPrice: updatedItem.buyPrice === "" ? item.buyPrice : updatedItem.buyPrice,
-                location: updatedItem.location === "" ? item.location : updatedItem.location,
-                billOfMaterial: updatedItem.billOfMaterial === "" ? item.billOfMaterial : updatedItem.billOfMaterial
-            })
-        }
-        return item;
-    };
-    inventoryItems.map(item => (
-        items.push({
-            id: item.id,
-            ItemName: item.itemName,
-            GoodType: item.goodType,
-            Quantity: item.quantity,
-            SellPrice: item.sellPrice,
-            BuyPrice: item.buyPrice,
-            Location: item.location,
-            BillOfMaterial: item.billOfMaterial,
-            modify: (updatedItem, toUpdate) => updateRow(item, updatedItem, toUpdate),
-            delete: () => deleteItem(item.id)
-        })));
+  let updateRow = (item, updatedItem, toUpdate) => {
+    if (toUpdate) {
+      updateItem({
+        id: item.id,
+        itemName: updatedItem.itemName === "" ? item.itemName : updatedItem.itemName,
+        goodType: updatedItem.goodType === "" ? item.goodType : updatedItem.goodType,
+        quantity: updatedItem.quantity === "" ? item.quantity : updatedItem.quantity,
+        sellPrice: updatedItem.sellPrice === "" ? item.sellPrice : updatedItem.sellPrice,
+        buyPrice: updatedItem.buyPrice === "" ? item.buyPrice : updatedItem.buyPrice,
+        location: updatedItem.location === "" ? item.location : updatedItem.location,
+        billOfMaterial: updatedItem.billOfMaterial === "" ? item.billOfMaterial : updatedItem.billOfMaterial
+      })
+    }
+    return item;
+  };
+  inventoryItems.map(item => (
+    items.push({
+      id: item.id,
+      ItemName: item.itemName,
+      GoodType: item.goodType,
+      Quantity: item.quantity,
+      SellPrice: item.sellPrice,
+      BuyPrice: item.buyPrice,
+      Location: item.location,
+      BillOfMaterial: item.billOfMaterial,
+      modify: (updatedItem, toUpdate) => updateRow(item, updatedItem, toUpdate),
+      delete: () => deleteItem(item.id)
+    })));
 
-    return (
-        <DataGrid rows={items} columns={inventoryCols} pageSize={9}/>
-    );
+  return (
+    <DataGrid rows={items} columns={inventoryCols} pageSize={9}/>
+  );
 };
 
 const Spinner = () => {
-    return (
-        <Box
-            display='flex'
-            flexDirection='column'
-            flexGrow={1}
-            style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <img src={AppLogo} alt='React Logo' className='App-logo'/>
-            <h1>Loading...</h1>
-        </Box>
-    );
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      flexGrow={1}
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img src={AppLogo} alt='React Logo' className='App-logo'/>
+      <h1>Loading...</h1>
+    </Box>
+  );
 };
 
 const LoadedView = ({classes, inventory}) => {
-    return (
-        <div>
-            <h1 style={{textAlign: "center"}}>Inventory</h1>
-            <div style={{height: 800, width: '85%', float: "left"}}>
-                <div style={{float: "left"}}>
-                    <InventoryForm
-                        initialButton='Insert'
-                        dialogTitle='Inventory Information '
-                        dialogContentText='Please enter information of the new item:'
-                        submitButton='Insert'
-                        onSubmit={(data) => insertItem(data)}
-                    />
-                </div>
-                <FilledInventoryView
-                    inventoryItems={inventory}
-                    classes={classes}
-                />
-            </div>
-
+  return (
+    <div>
+      <h1 style={{textAlign: "center"}}>Inventory</h1>
+      <div style={{height: 800, width: '85%', float: "left"}}>
+        <div style={{float: "left"}}>
+          <InventoryForm
+            initialButton='Insert'
+            dialogTitle='Inventory Information '
+            dialogContentText='Please enter information of the new item:'
+            submitButton='Insert'
+            onSubmit={(data) => insertItem(data)}
+          />
         </div>
-    );
+        <FilledInventoryView
+          inventoryItems={inventory}
+          classes={classes}
+        />
+      </div>
+
+    </div>
+  );
 };
 
 const SpinBeforeLoading = ({
-                               awaiting = async () => {
-                               },
-                               minLoadingTime = 0,
-                               ...props
+                             awaiting = async () => {
+                             },
+                             minLoadingTime = 0,
+                             ...props
                            }) => {
 
-    const [loading, setLoading] = useState(true);
-    const [loadingMin, setLoadingMin] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [loadingMin, setLoadingMin] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => setLoadingMin(false), minLoadingTime);
-        awaiting().then(() => setLoading(false));
-    }, [awaiting, minLoadingTime]);
+  useEffect(() => {
+    setTimeout(() => setLoadingMin(false), minLoadingTime);
+    awaiting().then(() => setLoading(false));
+  }, [awaiting, minLoadingTime]);
 
-    return loading || loadingMin ? spinnyBoi
-        : <Fragment>{props.children}</Fragment>;
+  return loading || loadingMin ? spinnyBoi
+    : <Fragment>{props.children}</Fragment>;
 };
 
 const Inventory = () => {
 
-    const classes = useStyles();
-    const [inventory, setInventory] = useState([]);
-    const waitForGetRequest = async () => getInventory().then(inv => setInventory(inv));
+  const classes = useStyles();
+  const [inventory, setInventory] = useState([]);
+  const waitForGetRequest = async () => getInventory().then(inv => setInventory(inv));
 
-    return (
-        <SpinBeforeLoading minLoadingTime={0} awaiting={waitForGetRequest}>
-            <LoadedView classes={classes} inventory={inventory}/>
-        </SpinBeforeLoading>
-    );
+  return (
+    <SpinBeforeLoading minLoadingTime={0} awaiting={waitForGetRequest}>
+      <LoadedView classes={classes} inventory={inventory}/>
+    </SpinBeforeLoading>
+  );
 };
 
 export {Inventory, FilledInventoryView, Spinner, SpinBeforeLoading};
