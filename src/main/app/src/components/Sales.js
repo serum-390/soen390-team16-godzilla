bd7e6ece5b56e7dd971e2e9bc125a573aacda064
Tashfia <46882997+tashfia98@users.noreply.github.com>
2021-03-03 20:07:36 -0500

    Updated Customer Management Section
    Added edit button.
    Added view button on the sales order

--- src/main/app/src/components/Sales.js
+++ src/main/app/src/components/Sales.js
@@ -5,19 +5,21 @@ import useSalesPageStyles from "../styles/salesPageStyles";
 import { SpinBeforeLoading } from "./inventory/Inventory";
 import  CustomerForm  from "../Forms/CustomerForm";
 
-
 const cols = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'vendorName', headerName: 'Customer', width: 130 },
-  { field: 'vendorAddress', headerName: 'Address', width: 130 },
-  { field: 'vendorPhone', headerName: 'Contact #', width: 130 },
+  { field: 'customerName', headerName: 'Customer', width: 130 },
+  { field: 'customerAddress', headerName: 'Address', width: 130 },
+  { field: 'customerPhone', headerName: 'Contact #', width: 130 },
   {
     field: 'salesOrders',
     headerName: 'Modify',
     width: 160,
     renderCell: params => (
+      
+
       <div style={{ margin: 'auto' }}>
         {
+
          /*<Button variant='contained'
           color='secondary'
           onClick={params.value.onClick}>
@@ -27,7 +29,14 @@ const cols = [
          initialButton='Edit' 
          dialogTitle='Customer Information '  
          dialogContentText='Please enter any information you would like to modify: ' 
+         customerName={params.getValue('customerName') || ''}
+         customerAddress={params.getValue('customerAddress').split(",")[0] || ''}
+         customerCity={params.getValue('customerAddress').split(",")[1] || ''}
+         customerPostal={params.getValue('customerAddress').split(",")[2] || ''}
+         customerProvince ={params.getValue('customerAddress').split(",")[3] || ''} //not yet done.
+         customerPhone={params.getValue('customerPhone') || ''}//not yet done.
          submitButton='Update'  
+         deleteButton='Delete' 
          /> 
         }  
       </div>
@@ -35,8 +44,9 @@ const cols = [
   },
 ];
 
+
 const getSales = async () => {
-  const api = '/api/sales/';
+  const api = '/api/Sales/';
   const got = await fetch(api);
   const json = await got.json();
 
@@ -44,11 +54,12 @@ const getSales = async () => {
 
   return json.sales.map(sale => {
     sale.salesOrders = {
-      onClick: () => alert(`Test: ${sale.vendorName}`),
+      onClick: () => alert(`Test: ${sale.customerName}`),
     };
     return sale;
   });
 };
+
 
 const SalesGrid = ({ className, columns, rows, onRowClick }) => {
   return (
@@ -66,16 +77,17 @@ const SalesGrid = ({ className, columns, rows, onRowClick }) => {
         </div>
         <DataGrid
           columns={columns}
-          rows={rows}
+          rows={customerRows}
           onRowClick={onRowClick}
         />
       </div>
-      <div style={{ height: 600, width: '45%', float: 'right' }}>
-      <div>
+      <div style={{ height: 600, width: '50%', float: 'right' }}>
+      <div >
           <h2 style={{ float: 'left' }}>Sales Orders</h2>
           <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={() => { AddNewSaleOrder(); }}>
             Add New Sales Order
           </Button>
+
       </div>
         <DataGrid
           columns={salesColumns}
@@ -87,19 +99,36 @@ const SalesGrid = ({ className, columns, rows, onRowClick }) => {
   );
 };
 
+const customerRows = [
+  { id: 1, customerName: 'Customer1', customerAddress: '1444 Rue Mackay,Montreal,H3G2M2,QC', customerPhone: "1234567000" },
+  { id: 2, customerName: 'Customer2', customerAddress: '666 Ontario St,Toronto,M4X1N1,ON', customerPhone: "1233207000" }
+];
+
 const salesColumns = [
-  { field: 'id', headerName: 'Order #', width: 130 },
+  { field: 'id', headerName: 'Order #', width: 110 },
   { field: 'customerName', headerName: 'Customer', width: 130 },
-  { field: 'items', headerName: 'Items', width: 130 },
+  { field: 'items', headerName: 'Products', width: 110 },
   { field: 'timestamp', headerName: 'Timestamp', width: 130 },
-  { field: 'cost', headerName: 'Cost', width: 130 }
+  { field: 'cost', headerName: 'Cost', width: 90 },
+  { field: 'status', headerName: 'Status', width: 110 },
+  { field: 'SalesOrderDetails',
+    headerName: 'Details',
+    width: 130,
+    renderCell: params => (
+      <div style={{ margin: 'auto' }}>
+        <Button variant="contained" color="primary" style={{ float: 'right' }} onClick={() => { AddNewSaleOrder(); }}>
+          View
+        </Button>
+      </div>
+    ),
+  }
 ];
 
 const salesRows = [
-  { id: 1, customerName: 'Customer 1', items: '????', timestamp: "01/31/2021", cost: "$100" },
-  { id: 2, customerName: 'Customer 1', items: '????', timestamp: "01/31/2021", cost: "$100" },
-  { id: 3, customerName: 'Customer 2', items: '????', timestamp: "01/31/2021", cost: "$200" },
-  { id: 4, customerName: 'Customer 3', items: '????', timestamp: "01/31/2021", cost: "$100" }
+  { id: 1, customerName: 'Customer 1', items: '????', timestamp: "01/31/2021", cost: "$100", status: "Completed" },
+  { id: 2, customerName: 'Customer 1', items: '????', timestamp: "01/31/2021", cost: "$100", status: "Completed"},
+  { id: 3, customerName: 'Customer 2', items: '????', timestamp: "01/31/2021", cost: "$200", status: "Ongoing"},
+  { id: 4, customerName: 'Customer 3', items: '????', timestamp: "01/31/2021", cost: "$100",status: "Ongoing" }
 ];
 
 
@@ -116,6 +145,7 @@ function Sales() {
 
   return (
     <SpinBeforeLoading minLoadingTime={500} awaiting={waitForGetRequest}>
+
       <SalesGrid
         className={classes.root}
         columns={cols}
