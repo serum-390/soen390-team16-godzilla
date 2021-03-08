91cca794dee36e7f122a314b2ac47bea4be02fbb
obonobo <ethanbnb@gmail.com>
2021-03-07 23:40:12 -0500

    Improved database config
    Populating the database will now run only once as opposed to everytime the
    application context is initialized.
    
    Added an environment variable that prevents the DB populating script from
    running at all, if that variable is set.
    
    Related work items: #149

--- src/main/java/ca/serum390/godzilla/config/DatabaseConfig.java
+++ src/main/java/ca/serum390/godzilla/config/DatabaseConfig.java
@@ -1,13 +1,10 @@
 package ca.serum390.godzilla.config;
 
-import ca.serum390.godzilla.util.JsonToMapConverter;
-import ca.serum390.godzilla.util.MapToJsonConverter;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
-import io.r2dbc.spi.ConnectionFactory;
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -20,16 +17,40 @@ import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.r2dbc.core.DatabaseClient;
 
-import java.util.ArrayList;
-import java.util.List;
+import ca.serum390.godzilla.util.JsonToMapConverter;
+import ca.serum390.godzilla.util.MapToJsonConverter;
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.log4j.Log4j2;
 
+/**
+ * ENV VARS:
+ *
+ * <ol>
+ * <li><code>DB_HOST:</code> The IP address or hostname of your DB></li>
+ * <li><code>DB_PORT:</code> The port that your DB is listening on></li>
+ * <li><code>DB_USERNAME:</code> The username for an account with access to the
+ * <code>godzilla</code> database</li>
+ * <li><code>DB_PASSWORD:</code> The password of the account</li>
+ * </ol>
+ */
 @Log4j2
 @Configuration
 @EnableR2dbcRepositories
-@AllArgsConstructor
 public class DatabaseConfig {
 
     private final ObjectMapper objectMapper;
+    private Environment environment;
+    private boolean skipPopulatingDatabase;
+    private static boolean databaseInitialized = false;
+
+    DatabaseConfig(ObjectMapper mapper, Environment env) {
+        this.objectMapper = mapper;
+        this.environment = env;
+        skipPopulatingDatabase = Boolean.parseBoolean(
+                environment.getProperty("SKIP_POPULATING_DATABASE"));
+    }
 
     /**
      * Creates a {@link DatabaseClient DatabaseClient Bean} that can be injected
@@ -52,12 +73,12 @@ public class DatabaseConfig {
     }
 
     @Bean
-    public ConnectionFactory connectionFactory(Environment env) {
+    public ConnectionFactory connectionFactory() {
 
-        String host = env.getProperty("DB_HOST");
-        int port = extractPortNumber(env, "DB_PORT");
-        String username = env.getProperty("DB_USERNAME");
-        String password = env.getProperty("DB_PASSWORD");
+        String host = environment.getProperty("DB_HOST");
+        int port = extractPortNumber("DB_PORT");
+        String username = environment.getProperty("DB_USERNAME");
+        String password = environment.getProperty("DB_PASSWORD");
 
         host = host != null ? host : "localhost";
         username = username != null ? username : "admin";
@@ -89,6 +110,11 @@ public class DatabaseConfig {
     public ConnectionFactoryInitializer connectionFactoryInitializer(ConnectionFactory connectionFactory) {
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
+
+        if (flipDatabaseInitializedFlag() || skipPopulatingDatabase) {
+            return initializer;
+        }
+
         CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
         populator.addPopulators(
                 new ResourceDatabasePopulator(new ClassPathResource("sql/schema.pgsql")),
@@ -97,13 +123,19 @@ public class DatabaseConfig {
         return initializer;
     }
 
-    private static Integer extractPortNumber(Environment env, String property) {
+    private Integer extractPortNumber(String property) {
         try {
-            String prop = env.getProperty(property);
+            String prop = environment.getProperty(property);
             return prop != null ? Integer.parseInt(prop) : 5432;
         } catch (NumberFormatException e) {
             log.error("Could not parse port number from environment variable DB_PORT: ", e);
             return 5432;
         }
+    }
+
+    private static boolean flipDatabaseInitializedFlag() {
+        boolean oldValue = databaseInitialized;
+        databaseInitialized = true;
+        return oldValue;
     }
 }
