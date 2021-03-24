cd8ab7660c1dfbf067ce78fd19160bce45289b65
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-03-24 15:28:54 -0400

    added proper list element for dark mode toggle

--- src/main/app/src/components/settingMiniPages/DisplaySettings.js
+++ src/main/app/src/components/settingMiniPages/DisplaySettings.js
@@ -1,8 +1,34 @@
+import { CircularProgress, Grid, MenuItem, MenuList, useMediaQuery, useTheme } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import useNavBarStyles from '../../styles/navBarSyles';
+import ThemeContext from '..//../App'
+import React, { useContext } from 'react';
+
+
+
 function DisplaySettings() {
+
+  const theme = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+
+  };
+
+
   return (
     <div>
-      <h1>This is the settings display page</h1>
-    </div>
+      <MenuList>
+        <MenuItem  >
+          <p>{JSON.stringify(theme)}</p>
+          <Grid container spacing={0} justify='flex-end' >
+            <Grid item key={0} style={{ padding: useTheme().spacing(1, 0) }}>
+              <Switch />
+            </Grid>
+          </Grid>
+        </MenuItem>
+      </MenuList>
+    </div >
   );
 }
 
