366212e94415658b44d384e8a68f919dc19df9cc
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-03-27 12:29:41 -0400

    Added seperation of dark and light mode colors

--- src/main/app/src/styles/themes.js
+++ src/main/app/src/styles/themes.js
@@ -6,13 +6,25 @@ const ThemeContext = React.createContext({});
 function useThisTheme() {
   const [darkMode, setDarkMode] = useState(false);
   const theme = useMemo(
-      () =>
-        createMuiTheme({
-          palette: {
-            type: darkMode ? 'dark' : 'light',
-          },
-        }),
-      [darkMode]);
+    () =>
+      createMuiTheme({
+        palette: {
+          type: darkMode ? 'dark' : 'light',
+          primary: darkMode ? {
+            light: '#484848',
+            main: '#757575',
+            dark: '#000000',
+            contrastText: '#fff',
+          } :
+            {
+              light: '#ffffff',
+              main: '#fafafa',
+              dark: '#c7c7c7',
+              contrastText: '#000',
+            },
+        },
+      }),
+    [darkMode]);
   return [theme, darkMode, setDarkMode];
 }
 
