7d303f02471161301cc533e4823198e1d6ee8130
thenafsters <nafisa.shamsuzzaman@gmail.com>
2021-04-03 14:27:14 -0400

    Removed Unused Imports

--- src/main/java/ca/serum390/godzilla/data/repositories/PackagedProductRepository.java
+++ src/main/java/ca/serum390/godzilla/data/repositories/PackagedProductRepository.java
@@ -1,23 +1,16 @@
 package ca.serum390.godzilla.data.repositories;
 
 import ca.serum390.godzilla.domain.packaging.PackagedProduct;
-import jdk.jfr.FlightRecorderListener;
 
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
-
-import javax.sound.sampled.FloatControl;
 
 @Repository
 public interface PackagedProductRepository extends ReactiveCrudRepository<PackagedProduct, Integer> {
     @Modifying
     @Query("UPDATE packaged_products SET LENGTH = $1, WIDTH = $2, HEIGHT= $3, WEIGHT=$4 , PACKAGE_TYPE = $6  WHERE ID = $5")
-    Mono<Integer> update(Float length, Float width, Float height, Float weight,
-                         String packageType, Integer id);
+    Mono<Integer> update(Float length, Float width, Float height, Float weight, String packageType, Integer id);
 }
