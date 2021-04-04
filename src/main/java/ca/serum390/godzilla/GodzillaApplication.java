cd26ee01ff93059348c6d42f9f55c5e2f8468ba3
amneet270 <amneet.s.270@gmail.com>
2021-04-04 19:37:57 -0400

    fixed miror error

--- src/main/java/ca/serum390/godzilla/GodzillaApplication.java
+++ src/main/java/ca/serum390/godzilla/GodzillaApplication.java
@@ -6,14 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GodzillaApplication {
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		SpringApplication.run(GodzillaApplication.class, args);
-		
-	}
-=======
     public static void main(String[] args) {
         SpringApplication.run(GodzillaApplication.class, args);
     }
->>>>>>> fd193d56c737d4ec9714881f156abff54329dbf9
 }
