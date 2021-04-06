cd1370167b2b3ed6db8fda52a4ed2a3783565fae
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-04-05 21:15:21 -0400

    Navbar

--- src/main/app/src/components/helpMiniPages/accountingHelp.js
+++ src/main/app/src/components/helpMiniPages/accountingHelp.js
@@ -21,7 +21,7 @@ function AccountingHelp() {
       <h2>Exporting as CSV</h2>
       <h4>Transactions can easily be expored as an external .csv file for usage.</h4>
       <img src={exportImage} alt="img" style={{ width: '70%', height: '70%', resizeMode: 'contain' }} />
-      <h4>To Export as a .csv file , click on the "Export" button, and then "Download as CSV". This will immediately begin a download of an exported CSV file to your device</h4>
+      <h4>To Export as a .csv file , click on the "Export" button, and then "Download as CSV". This will immediately begin a download of an exported CSV file to your device.</h4>
 
     </div>
   );
