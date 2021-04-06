c8c8a2bfbfe24eff7870f859e5d3744c4d845829
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-04-05 21:21:25 -0400

    Production

--- src/main/app/src/components/helpMiniPages/ProductionHelp.js
+++ src/main/app/src/components/helpMiniPages/ProductionHelp.js
@@ -1,9 +1,20 @@
 import exportImage from '../../misc/helpPageImages/exporting.png';
 
+import orderingImage from '../../misc/helpPageImages/ordering.png';
+
 function ProductionHelp() {
   return (
     <div>
-      <h1>This is the ProductionHelp page</h1>
+      <h1>Production</h1>
+      <h4>The Production Page displays a list of all items in the production line until the items are completed. This list consists of :</h4>
+      <ul>
+        <li>Raw materials</li>
+        <li>Products under assembly</li>
+      </ul>
+      <h4>All completed items will be removed from the production line and displayed in the inventory page</h4>
+      <h2>Reordering of tables</h2>
+      <h4>Tables can be reordered in different orders by selecting the corresponding column title</h4>
+      <img src={orderingImage} alt="img" style={{ width: '10%', height: '10%', resizeMode: 'contain' }} />
       <h2>Exporting as CSV</h2>
       <h4>Both Materials and Products can easily be expored as an external .csv file for usage.</h4>
       <img src={exportImage} alt="img" style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
